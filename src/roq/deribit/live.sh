e9581eb2f0ab0afe9bd0b6fde8a31fe021a9855a
thraneh <thraneh@gmail.com>
2019-09-26 04:03:57 +0000

    websocket is not needed at this point

--- src/roq/deribit/live.sh
+++ src/roq/deribit/live.sh
@@ -14,14 +14,13 @@ CONFIG_FILE="$CWD/config/$NAME.toml"
 
 URI="deribit.com"
 
-WS_URI="wss://$URI/ws/api/v2"
 FIX_URI="tcp://www.$URI:9880"
+# WS_URI="wss://$URI/ws/api/v2"
 
 $PREFIX ./roq-deribit \
 	--name "$NAME" \
 	--config-file "$CONFIG_FILE" \
 	--fix-uri "$FIX_URI" \
-	--ws-uri "$WS_URI" \
 	--listen "$CWD/$NAME.sock" \
 	--metrics "$CWD/$NAME_metrics.sock" \
 	$@
