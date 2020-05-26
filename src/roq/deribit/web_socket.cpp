ad5f70f328c3d5d5c126ca99e6686ab8ddbeadb4
thraneh <thraneh@gmail.com>
2020-05-26 16:47:30 +0200

    support web-socket query params

--- src/roq/deribit/web_socket.cpp
+++ src/roq/deribit/web_socket.cpp
@@ -61,6 +61,7 @@ WebSocket::WebSocket(
           dns_base,
           ssl_context,
           core::URI(FLAGS_ws_uri),
+          std::string_view(),  // query
           std::chrono::seconds { FLAGS_ws_ping_freq_secs },
           FLAGS_decode_buffer_size,  // XXX need read buffer size
           FLAGS_encode_buffer_size,
