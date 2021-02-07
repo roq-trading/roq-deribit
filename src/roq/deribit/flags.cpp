e25d80e5d03f5d3b25d4bd0e20f8fd4ff1b99fa4
thraneh <thraneh@gmail.com>
2021-02-07 06:29:38 +0100

    validation of flags

--- src/roq/deribit/flags.cpp
+++ src/roq/deribit/flags.cpp
@@ -7,45 +7,101 @@
 
 #include <string>
 
-ABSL_FLAG(std::string, config_file, "", "config file (path)");
+#include "roq/core/flags/non_empty.h"
+#include "roq/core/flags/non_zero.h"
+#include "roq/core/flags/path.h"
+#include "roq/core/flags/uri.h"
 
-ABSL_FLAG(std::string, exchange, "deribit", "exchange identifier (string)");
+ABSL_FLAG(  //
+    roq::core::flags::Path<std::string>,
+    config_file,
+    {},
+    "config file (path)");
+
+ABSL_FLAG(  //
+    roq::core::flags::NonEmpty<std::string>,
+    exchange,
+    {"deribit"},
+    "exchange identifier (string)");
 
 // ws
 
-ABSL_FLAG(std::string, ws_uri, "wss://test.deribit.com/ws/api/v2", "WebSocket end-point (URI)");
+ABSL_FLAG(  //
+    roq::core::flags::URI<std::string>,
+    ws_uri,
+    {"wss://test.deribit.com/ws/api/v2"},
+    "WebSocket end-point (URI)");
 
-ABSL_FLAG(uint32_t, ws_ping_freq_secs, 5, "ping frequency (seconds)");
+ABSL_FLAG(  //
+    roq::core::flags::NonZero<uint32_t>,
+    ws_ping_freq_secs,
+    uint32_t{5},
+    "ping frequency (seconds)");
 
-ABSL_FLAG(uint32_t, ws_request_timeout_secs, 15, "request timeout (seconds)");
+ABSL_FLAG(  //
+    uint32_t,
+    ws_request_timeout_secs,
+    uint32_t{15},
+    "request timeout (seconds)");
 
 // fix
 
-ABSL_FLAG(std::string, fix_uri, "tcp://test.deribit.com:9881", "FIX end-point (URI)");
+ABSL_FLAG(  //
+    roq::core::flags::URI<std::string>,
+    fix_uri,
+    {"tcp://test.deribit.com:9881"},
+    "FIX end-point (URI)");
 
-ABSL_FLAG(uint32_t, fix_ping_freq_secs, 5, "ping frequency (seconds)");
+ABSL_FLAG(  //
+    roq::core::flags::NonZero<uint32_t>,
+    fix_ping_freq_secs,
+    uint32_t{5},
+    "ping frequency (seconds)");
 
-ABSL_FLAG(uint32_t, fix_request_timeout_secs, 15, "request timeout (seconds)");
+ABSL_FLAG(  //
+    uint32_t,
+    fix_request_timeout_secs,
+    uint32_t{15},
+    "request timeout (seconds)");
 
-ABSL_FLAG(bool, fix_cancel_on_disconnect, true, "cancel orders on disconnect? (bool)");
+ABSL_FLAG(  //
+    bool,
+    fix_cancel_on_disconnect,
+    true,
+    "cancel orders on disconnect? (bool)");
 
-ABSL_FLAG(
-    uint32_t, fix_market_data_request_max_size, 56, "max batch size (it appears there is a limit)");
+ABSL_FLAG(  //
+    uint32_t,
+    fix_market_data_request_max_size,
+    uint32_t{56},
+    "max batch size (it appears there is a limit)");
 
-ABSL_FLAG(bool, fix_debug, false, "log fix messages?");
+ABSL_FLAG(  //
+    bool,
+    fix_debug,
+    false,
+    "log fix messages?");
 
 // XXX review
 
-ABSL_FLAG(uint32_t, encode_buffer_size, 1048576, "encode buffer size");
+ABSL_FLAG(  //
+    roq::core::flags::NonZero<uint32_t>,
+    encode_buffer_size,
+    uint32_t{1048576},
+    "encode buffer size");
 
-ABSL_FLAG(uint32_t, decode_buffer_size, 10485760, "decode buffer size");
+ABSL_FLAG(  //
+    roq::core::flags::NonZero<uint32_t>,
+    decode_buffer_size,
+    uint32_t{10485760},
+    "decode buffer size");
 
 // external
 
-ABSL_DECLARE_FLAG(std::string, name);
-ABSL_DECLARE_FLAG(uint32_t, cache_mbp_max_depth);
-ABSL_DECLARE_FLAG(uint32_t, cache_trades_max_depth);
-ABSL_DECLARE_FLAG(uint32_t, cache_fills_max_depth);
+ABSL_DECLARE_FLAG(roq::core::flags::NonEmpty<std::string>, name);
+ABSL_DECLARE_FLAG(roq::core::flags::NonZero<uint32_t>, cache_mbp_max_depth);
+ABSL_DECLARE_FLAG(roq::core::flags::NonZero<uint32_t>, cache_trades_max_depth);
+ABSL_DECLARE_FLAG(roq::core::flags::NonZero<uint32_t>, cache_fills_max_depth);
 
 namespace roq {
 namespace deribit {
