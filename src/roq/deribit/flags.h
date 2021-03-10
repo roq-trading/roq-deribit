ae44e7aa75cb7445d49932d7e5a8155d156b8875
thraneh <thraneh@gmail.com>
2021-03-10 11:52:25 +0100

    ensure all usage of flags (using abseil-cpp) will compile with the default standard version

---
+++ src/roq/deribit/flags.h
@@ -0,0 +1,13 @@
+/* Copyright (c) 2017-2021, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/deribit/flags/flags.h"
+
+namespace roq {
+namespace deribit {
+
+using flags::Flags;
+
+}  // namespace deribit
+}  // namespace roq
