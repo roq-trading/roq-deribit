764c7b4e78fb1c94787780cb3fadcae1411924da
thraneh <thraneh@gmail.com>
2021-07-04 14:18:16 +0200

    magic_enum::enum_values also needed for some use-cases

--- src/roq/deribit/shared.cpp
+++ src/roq/deribit/shared.cpp
@@ -2,8 +2,6 @@
 
 #include "roq/deribit/shared.h"
 
-#include <magic_enum.hpp>
-
 #include "roq/deribit/flags.h"
 
 namespace roq {
@@ -12,7 +10,7 @@ namespace deribit {
 Shared::Shared(server::Dispatcher &dispatcher)
     : fills(server::Flags::cache_fills_max_depth()), bids(server::Flags::cache_mbp_max_depth()),
       asks(server::Flags::cache_mbp_max_depth()), trades(server::Flags::cache_trades_max_depth()),
-      statistics(magic_enum::enum_count<StatisticsType::type_t>()), dispatcher_(dispatcher) {
+      statistics(StatisticsType::count()), dispatcher_(dispatcher) {
 }
 
 std::string_view Shared::next_request_id() {
