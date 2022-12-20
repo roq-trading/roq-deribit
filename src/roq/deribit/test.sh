d53ece9a2e635868e8153e49b46c60f4b83e9ef8
thraneh <thraneh@gmail.com>
2022-12-20 05:58:32 +0100

    json numbers now formatted using context providing price/quantity decimals

--- src/roq/deribit/test.sh
+++ src/roq/deribit/test.sh
@@ -39,6 +39,7 @@ $PREFIX "./roq-deribit" \
   --event_log_symlink true \
   --client_listen_address "$HOME/run/$NAME.sock" \
   --service_listen_address 1234 \
+  --url_prefix roq \
   --fix_uri "$FIX_URI" \
   --ws_uri "$WS_URI" \
   $@
