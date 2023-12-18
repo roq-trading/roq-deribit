e47f3808e7004113e04b3ed11e57a14f9429d5b7
thraneh <thraneh@gmail.com>
2023-12-18 11:27:54 +0100

    just check --help

--- conda/run_test.sh
+++ conda/run_test.sh
@@ -36,6 +36,8 @@ if roq-deribit --help; then
   (>&2 echo -e "\033[1;31mERROR: Unexpected error code.\033[0m") && exit 1
 fi
 
-roq-deribit-benchmark
+if ! roq-deribit-benchmark --help; then
+  (>&2 echo -e "\033[1;31mERROR: Unexpected error code.\033[0m") && exit 1
+fi
 
 echo -e "\033[1;34m--- DONE ---\033[0m"
