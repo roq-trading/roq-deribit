4167f7e62eef47e91343d4500bdd329ef5e71b21
thraneh <thraneh@gmail.com>
2024-01-07 06:17:15 +0100

    always log flags

--- src/roq/deribit/settings.cpp
+++ src/roq/deribit/settings.cpp
@@ -13,7 +13,7 @@ Settings::Settings(args::Parser const &args)
     : server::flags::Settings{args, ROQ_PACKAGE_NAME, ROQ_BUILD_NUMBER}, flags::Flags{flags::Flags::create()},
       common{flags::Common::create()}, fix{flags::FIX::create()}, ws{flags::WS::create()},
       multicast{flags::Multicast::create()} {
-  log::debug("settings={}"sv, *this);
+  log::info("settings={}"sv, *this);
 }
 
 }  // namespace deribit
