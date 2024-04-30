0890717664034d82a3c027306bdd996e7bf24434
thraneh <thraneh@gmail.com>
2024-04-30 16:00:17 +0200

    chore

--- src/roq/deribit/order_entry.cpp
+++ src/roq/deribit/order_entry.cpp
@@ -863,7 +863,7 @@ void OrderEntry::operator()(Trace<fix::ExecutionReport> const &event, roq::fix::
           .price = item.fill_px,
           .liquidity = liquidity,
           .quote_quantity = NaN,
-          .commission_quantity = NaN,
+          .commission_quantity = NaN,  // note! we only have it per TRADE
           .commission_currency = {},
       };
       fills.emplace_back(std::move(fill));
