34a2b252a3071ae592e736449e3e2cd1b0e4c0a8
thraneh <thraneh@gmail.com>
2024-10-14 04:54:03 +0200

    adding cfi_code to reference data

--- src/roq/deribit/market_data.cpp
+++ src/roq/deribit/market_data.cpp
@@ -662,6 +662,7 @@ void MarketData::operator()(Trace<fix::SecurityList> const &event, roq::fix::Hea
           .symbol = symbol,
           .description = item.security_desc,
           .security_type = security_type,
+          .cfi_code = {},
           .base_currency = item.settl_currency,
           .quote_currency = item.currency,
           .settlement_currency = item.settl_currency,
