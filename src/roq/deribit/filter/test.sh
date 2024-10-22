2be79924b31b9a51582f138b6477fd34d4d7f953
thraneh <thraneh@gmail.com>
2024-10-22 09:51:57 +0200

    update

--- src/roq/deribit/filter/test.sh
+++ src/roq/deribit/filter/test.sh
@@ -5,4 +5,4 @@ CONFIG_FILE="$HOME/dev/roq-dev/roq-deribit/share/prod/channels.json"
 ./roq-deribit-filter \
   --type tcpdump \
   --multicast_config_file $CONFIG_FILE \
-  --multicast_channel_ids 1
+  --multicast_channel_ids 1,2,3,4,7,10,13,21
