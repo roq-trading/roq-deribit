9e76b1991334d0c1313779a2f0260c827f46823b
thraneh <thraneh@gmail.com>
2025-08-20 04:38:56 +0200

    update

--- src/roq/deribit/test.sh
+++ src/roq/deribit/test.sh
@@ -76,6 +76,6 @@ $PREFIX "./roq-deribit" \
   --enable_portfolio=true \
   --time_series_interval "60s" \
   --time_series_lookback "2h" \
-  --time_series_realtime true \
-  --download_time_series true \
+  --time_series_realtime=true \
+  --download_time_series=false \
   $@
