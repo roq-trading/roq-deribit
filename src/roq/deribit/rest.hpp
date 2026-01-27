44a9fb3ff91282f09d4f3a5d0ac9552b8c927b5e
thraneh <thraneh@gmail.com>
2026-01-27 08:13:45 +0100

    prepare dynamic subscriptions

--- src/roq/deribit/rest.hpp
+++ src/roq/deribit/rest.hpp
@@ -34,7 +34,7 @@ struct Rest final : public web::rest::Client::Handler {
   };
 
   struct SymbolsUpdate final {
-    std::vector<Symbol> &symbols;
+    std::span<Symbol const> symbols;
   };
 
   struct Handler {
@@ -121,7 +121,6 @@ struct Rest final : public web::rest::Client::Handler {
   } latency_;
   // cache
   Shared &shared_;
-  utils::unordered_set<std::string> symbols_;
   // state
   ConnectionStatus status_ = {};
   //
